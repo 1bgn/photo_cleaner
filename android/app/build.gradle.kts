d67531c1785b17eac95c600c32d3dacc23a6e1e1
1bgn <137element@gmail.com>
2026-01-28 13:03:22 +0800

    add appsflyer

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 
-    id("com.google.gms.google-services")
+//    id("com.google.gms.google-services")
 }
 
 android {
@@ -26,7 +26,7 @@ android {
         applicationId = "com.nicfuno.sonicforgeflow"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,44 @@ import 'package:flutter/material.dart';
 import 'package:photo_cleaner/core/di/di.dart';
 import 'package:photo_cleaner/features/editor/presentation/editor_screen.dart';
 import 'package:photo_cleaner/features/editor/presentation/editor_screen.dart';
+import 'package:appsflyer_sdk/appsflyer_sdk.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  await Apphud.start(apiKey: "app_Z44sHCCXqhP5FCBDa8SxKBLB7VLpga");
-
+  await initAppsFlyer();
+  await initApphud();
   configureDependencies();
   runApp(EasyLocalization(child: const MyApp(), supportedLocales: [Locale("ru"),Locale("en")],fallbackLocale: Locale("ru"), path: 'assets/translations'));
+}
+Future<void> initApphud() async {
+  await Apphud.start(apiKey: "app_Z44sHCCXqhP5FCBDa8SxKBLB7VLpga",);
+
+}
+Future<void> initAppsFlyer()async {
+  final AppsFlyerOptions options = AppsFlyerOptions(
+      afDevKey: "GAgckFyN4yETigBtP4qtRG",
+      appId: "6749377146",
+      showDebug: true,
+      timeToWaitForATTUserAuthorization: 15,
+      manualStart: true);
+  AppsflyerSdk _appsflyerSdk = AppsflyerSdk(options);
+
+
+  await _appsflyerSdk.initSdk(
+      registerConversionDataCallback: true,
+      registerOnAppOpenAttributionCallback: true,
+      registerOnDeepLinkingCallback: true);
+
+
+  _appsflyerSdk.startSDK(
+    onSuccess: () {
+      print("AppsFlyer SDK initialized successfully.");
+    },
+    onError: (int errorCode, String errorMessage) {
+      print("Error initializing AppsFlyer SDK: Code $errorCode - $errorMessage");
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
